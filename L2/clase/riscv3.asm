e9ae09ba2cc8f10da18ad51b6e150da794d4ba1d
Borjatis <borja.ca@gmail.com>
2022-10-14 14:27:44 +0200

    Cambios y clase L2

---
+++ L2/clase/riscv3.asm
@@ -0,0 +1,8 @@
+#-- Programa con dos contadores 
+#-- El registro x5 se incrementa de uno en uno
+#-- El registro x6 se incrementa de dos en dos
+
+	.text
+	
+	
+#. test es una instruccion para el ensamblador, no genera codigo fuente
