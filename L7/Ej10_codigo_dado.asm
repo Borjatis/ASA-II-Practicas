016b7f22f06f315d3663c8e124b7f5fc829e5813
Borjatis <borja.ca@gmail.com>
2022-11-16 14:27:02 +0100

    Haciendo ejers del laboratorio 7

---
+++ L7/Ej10_codigo_dado.asm
@@ -0,0 +1,9 @@
+      jal x0, next         
+      addi x6,x0,1      
+      addi x7,x0,2      
+      addi x28,x0,3     
+      addi x29,x0,4
+next:     
+      addi x30,x0,5     
+      addi x17,x0,10    
+      ecall
